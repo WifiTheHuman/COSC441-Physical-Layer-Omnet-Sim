773a7bb77988afbc289638c5050ab6ce13e17564
Yifei Ma <yma82@cs19110bs.canterbury.ac.nz>
2020-09-01 04:18:53 +1200

    added debugging messages

--- src/Channel.cc.cc
+++ src/Channel.cc.cc
@@ -26,25 +26,41 @@ public:
 private:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    double nodeDistance;
-    double pathLossExponent;
-    double txPowerDBm;
-    double bitRate;
-    double noisePowerDBm;
-    double transProbGoodGood;
-    double transProbBadBad;
-    double channelGainGoodDB;
-    double channelGainBadDB
+    double* nodeDistance = new double;
+    double* pathLossExponent = new double;
+    double* txPowerDBm = new double;
+    double* bitRate = new double;
+    double* noisePowerDBm = new double;
+    double* transProbGoodGood = new double;
+    double* transProbBadBad = new double;
+    double* channelGainGoodDB = new double;
+    double* channelGainBadDB = new double;
 };
 
 Define_Module(Channel_cc);
 
 void Channel_cc::initialize()
 {
-    // TODO - Generated method body
+    cout<<"Channel initialising, nodeDistance = "<<*nodeDistance;
+    cout<<"Channel initialising, pathLossExponent = "<<*pathLossExponent;
+    cout<<"Channel initialising, txPowerDBm = "<<*txPowerDBm;
+    cout<<"Channel initialising, bitRate = "<<*bitRate;
+    cout<<"Channel initialising, noisePowerDBm = "<<*noisePowerDBm;
+    cout<<"Channel initialising, transProbGoodGood = "<<*transProbGoodGood;
+    cout<<"Channel initialising, transProbBadBad = "<<*transProbBadBad;
+    cout<<"Channel initialising, channelGainGoodDB = "<<*channelGainGoodDB;
+    cout<<"Channel initialising, channelGainBadDB = "<<*channelGainBadDB;
 }
 
 void Channel_cc::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    cout<<"Channel initialising, nodeDistance = "<<*nodeDistance;
+        cout<<"Channel handling message, pathLossExponent = "<<*pathLossExponent;
+        cout<<"Channel handling message, txPowerDBm = "<<*txPowerDBm;
+        cout<<"Channel handling message, bitRate = "<<*bitRate;
+        cout<<"Channel handling message, noisePowerDBm = "<<*noisePowerDBm;
+        cout<<"Channel handling message, transProbGoodGood = "<<*transProbGoodGood;
+        cout<<"Channel handling message, transProbBadBad = "<<*transProbBadBad;
+        cout<<"Channel handling message, channelGainGoodDB = "<<*channelGainGoodDB;
+        cout<<"Channel handling message, channelGainBadDB = "<<*channelGainBadDB;
 }
